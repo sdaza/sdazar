3b7d7c7555a7e4fdc298e718efbfbf79dfdd0a3b
sdaza <sebastian.daza@gmail.com>
2015-05-17 15:25:33 -0500

    update

--- R/formulas.R
+++ R/formulas.R
@@ -1,5 +1,5 @@
 #' @title Formulas for linear models.
-#' @description Formulas for linear models. 
+#' @description Formulas for linear models.
 #' @param var1 Dependent variable (character).
 #' @param var2 Independent variables (character).
 #' @param sym1 First operator (between dependent and independent variables)
@@ -7,7 +7,7 @@
 #' @return Formula object.
 #' @examples
 #' # formulas(c("var1", "var2", "~", "+"))
-formulas <- function(var1, var2, sym1, sym2) {
-f <- as.formula(paste0(var1, sym1, paste0(paste0(var, collapse = sym2))))
+formulas <- function(var1, var2, sym1 = "~", sym2 = "+") {
+f <- as.formula(paste0(var1, sym1), paste0(paste0(var, collapse = sym2)))
 return(f)
 }
