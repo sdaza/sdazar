7b53a3fa93b3326ec2f4f560f37dda3ae98289d5
sdaza <sdaza@sdaza.local>
2015-06-25 11:45:25 -0500

    update

---
+++ R/outliers.R
@@ -0,0 +1,21 @@
+#' @title Identify outliers.
+#' @description Function to identify outliers
+#' @param c Thresold value.
+#' @param values Logical. If it is \code{TRUE}, will show outlier values.
+#' @examples
+#' outliers(x = c(1,2,3,4,100,21000,20021, -122332), values = TRUE)
+outliers <- function(x, c = 1.5, values = FALSE) {
+  x <- na.omit(x)
+  q1 <- quantile(x, .25)
+  q3 <- quantile(x, .75)
+  iqd <- q3 - q1
+  val <- x[ (x <= (q1 - c * iqd))  | (x >= ( q3 + c * iqd))]
+  ind <- which((x <= (q1 - c * iqd)) | (x >= ( q3 + c * iqd)))
+
+  if (values == TRUE) {
+    return(val)
+  }
+  else {
+    return(ind)
+  }
+}
