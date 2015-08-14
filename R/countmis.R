8cf83c8f8f7de1ee1bd100634c70ed3b8ac72abe
Sebastian Daza <sebastian.daza@gmail.com>
2015-08-14 16:42:13 -0500

    update

--- R/countmis.R
+++ R/countmis.R
@@ -9,7 +9,7 @@ countmis  <- function(dat, pct = TRUE) {
 mis <- sort( sapply(dat, function(x) sum(is.na(x))), decreasing = TRUE)
 
 if (pct == FALSE) { return(mis) } 
-else if ( pct == TRUE ) {return(round(mis / nrow(dat), 3)) } 
+else if ( pct == TRUE ) { return( round(mis / nrow(dat), 3)) } 
 
 }
 
