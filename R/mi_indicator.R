e09117ae64aab6767bcf66ab8ae34b635c333980
Sebastian <sebastian.daza@gmail.com>
2015-09-30 10:26:54 -0500

    update

--- R/mi_indicator.R
+++ R/mi_indicator.R
@@ -12,20 +12,20 @@
 #' mi_indicator(dat, vars)
 mi_indicator <- function(dat, vars = NULL) {
 
-if (is.null(vars)) {
-  vars <- names(dat)
-}
+  if (is.null(vars)) {
+    vars <- names(dat)
+  }
 
-dat <- data.table(dat)
-nvars <- names(countmis(dat, vars, exclude.complete = TRUE))
-mivars <- paste0("mi_", nvars) 
+  dat <- data.table(dat)
+  nvars <- names(countmis(dat, vars, exclude.complete = TRUE))
+  mivars <- paste0("mi_", nvars) 
 
-for (i in 1:length(nvars)) {
+  for (i in 1:length(nvars)) {
 
-  dat[, mivars[i] := ifelse(is.na(dat[, nvars[i], with = FALSE]), 1, 0)]
+    dat[, mivars[i] := ifelse(is.na(dat[, nvars[i], with = FALSE]), 1, 0)]
 
-}
+  }
 
-return(dat)
+  return(dat)
 
-}
+  }
