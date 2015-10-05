50b58f9ee8fd8f1cc845f94d9134250d4304f38f
Sebastian <sebastian.daza@gmail.com>
2015-10-05 13:37:04 -0500

    update

--- R/knitpost.R
+++ R/knitpost.R
@@ -1,10 +1,10 @@
 #' @title Create posts from Rmd files for blogging.
 #' @description Create posts from .Rmd files for blogging.
 #' @param input Path of Rmd file.
-KnitPost <- function(input, base.url="/") {
+KnitPost <- function(input, base.url="/", plot.path = NULL) {
   opts_knit$set(base.url = base.url)
   
-  fig.path <- paste0("figs/", sub(".Rmd$", "", basename(input)), "/")
+  fig.path <- paste0(plot.path, sub(".Rmd$", "", basename(input)), "/")
   opts_chunk$set(fig.path = fig.path)
   opts_chunk$set(fig.cap = "center")
 
