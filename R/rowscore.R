773211ba6d43498b180c91eafd5d66836e705785
Sebastian <sebastian.daza@gmail.com>
2015-10-07 16:32:54 -0500

    update

--- R/rowscore.R
+++ R/rowscore.R
@@ -2,7 +2,7 @@
 #' @description Create scores: mean or if any of the variables have specific values.
 #' @param dat `data.table` object.
 #' @param vars Character vector with name of variables.
-#' @param type Type of score (`mean`, `any`, `parcel`). `parcel` selects the items with less missing data based on `p` (once the items are define a mean is computed using complete cases).
+#' @param type Type of score (`mean`, `any`, `parcel`). `parcel` selects the items with less missing data based on `p` (once the items are defined a mean is computed using complete cases).
 #' @param p  Proportion of items with valid values to compute mean.
 #' @param nitems Number of items with valid values to compute mean.
 #' @param values Values used to define `any` index.
@@ -14,8 +14,10 @@
 #' dat <- data.table(a,b,c)
 #' vars <- names(dat)
 #' rowscore(dat, vars)
-rowscore <- function (dat, vars, type = "mean", p = 0.5, nitems = NULL, val = NULL)
-{
+rowscore <- function (dat, vars, type = "mean", p = 0.5, nitems = NULL, val = NULL) {
+temp <- data.table(dat)
+temp <- temp[, vars, with = FALSE]
+
   if (is.null(val) & type == "any") {
     stop("`val` need to be specified wheh using `any`")
   }
@@ -23,44 +25,42 @@ rowscore <- function (dat, vars, type = "mean", p = 0.5, nitems = NULL, val = NU
     stop("`vars` has to be a character vector")
   }
   if (!is.na(p) & is.null(nitems)) {
-  dat[, z := apply(dat[, vars, with = FALSE], 1, function(x) sum(!is.na(x)) / length(x))]
+  temp[, z := apply(temp, 1, function(x) sum(!is.na(x)) / length(x))]
 }
 
   if (!is.null(nitems)) {
- dat[, z := apply(dat[, vars, with = FALSE], 1, function(x) sum(!is.na(x)))]
+ temp[, z := apply(temp, 1, function(x) sum(!is.na(x)))]
   p <-  nitems
    }
 
   if (type == "mean") {
-    out <- dat[, ifelse( z >= p, apply( dat[, vars, with = FALSE], 1, mean, na.rm = TRUE), NA)]
+    out <- temp[, ifelse( z >= p, apply( temp, 1, mean, na.rm = TRUE), NA)]
     # remove this residual variable
-     dat[, z := NULL]
+     temp[, z := NULL]
      }
 
   if (type == "any") {
-    out <- dat[, ifelse( apply(dat[, vars, with = FALSE], 1,
-                               function (x) all( is.na(x) ) ), NA, apply(dat[, vars, with = FALSE], 1, function(x) as.numeric( any(x %in% val)) ))][, out]
+    out <- temp[, ifelse( apply(temp, 1,
+                               function (x) all( is.na(x) ) ), NA, apply(temp, 1, function(x) as.numeric( any(x %in% val)) ))]
 
   }
+
     if (type == "parcel") {
 
     nitems <- trunc( length(vars) * p)
-    # select items with less missing data
-    pnames <- names(rev(countmis(w1, vars, exclude.complete = FALSE))[1:nitems])
 
-    temp <- dat[, tid := 1:.N][, c(" tid", vars), with = FALSE]
+    # select items with less missing data
+    pnames <- names(rev(countmis(temp, vars, exclude.complete = FALSE))[1:nitems])
+
+    temp[, tid := 1:.N]
     numerics(temp, vars)
     mtemp <- melt(temp, id = "tid", measure = vars)
     setkey(mtemp, tid)
 
-    # remove this residual variable
-    dat[, tid := NULL]
-
-    # complete cases computation
+    # complete cases mean computation
     out <- mtemp[variable %in% pnames, .(mvalue = mean(value)), by = tid][, mvalue]
 
   }
-
 
   return(out)
 
