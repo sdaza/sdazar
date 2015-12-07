f9d42aa5b7ba38d2c6ce742b3da403991c18e296
Sebastian <sebastian.daza@gmail.com>
2015-12-07 08:38:46 -0600

    update

--- R/getdummies.R
+++ R/getdummies.R
@@ -10,14 +10,24 @@
 #' dat <- data.table(a,b,c)
 #' vars <- names(dat)
 #' getdummies(dat, "a")
-getdummies <- function(dat, vars){
+getdummies <- function(dat, vars) {
+
+  # checks
   stopifnot(is.data.table(dat))
   stopifnot(vars %in% names(dat))
-  factors(dat, vars)
 
-  dat[, paste0(vars, "." , levels(get(vars)))] -> new.names
-  dat[, (new.names) := transpose(lapply(get(vars),
-                                        FUN = function(x) { as.numeric(x == levels(get(vars)))})) ]
-  cat(paste("\nNew variables: ", paste0(new.names, collapse = ", ")))
+  # create lists for loop
+  inds <- list()
+  nvars <- list()
+
+  for (i in 1:length(vars)) {
+    inds[[i]] <- na.omit(unique(dat[, vars[i], with = FALSE]))[[1]]
+    nvars[[i]] <- paste0(vars[i], ".", inds[[i]])
+    dat[, (nvars[[i]]) := lapply(inds[[i]], function(x) as.numeric(get(vars[i]) == x))]
+  }
+
+  # print variables names to check
+  print(nvars)
   return(dat)
+
 }
